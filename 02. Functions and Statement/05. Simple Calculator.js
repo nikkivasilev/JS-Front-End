e9e7ed947ccbcb1d48685435064e61348d8c1d21
nikkivasilev <n.vasilev105@gmail.com>
2023-03-01 20:42:11 +0200

    solved some simple JS exercises

--- 02. Functions and Statement/05. Simple Calculator.js
+++ 02. Functions and Statement/05. Simple Calculator.js
@@ -1,3 +1,19 @@
-function calculator(params) {
-    
+function calculator(firstNum, secondNum, operation) {
+    const add = (a, b) => a + b;
+    const subtract = (a, b) => a - b;
+    const divide = (a, b) => a / b;
+    const multiply = (a, b) => a * b;
+    switch (operation) {
+        case 'add':
+            return add(firstNum, secondNum)
+        case 'subtract':
+            return subtract(firstNum, secondNum)
+        case 'divide':
+            return divide(firstNum, secondNum)
+        case 'multiply':
+            return multiply(firstNum, secondNum)
+        default:
+            console.log('Invalid Operation!')
+            break;
+    }
 }
--- 02. Functions and Statement/6. Sign Check.js
+++ 02. Functions and Statement/6. Sign Check.js
@@ -1,3 +1,20 @@
 function singCheck(numOne, numTwo, numThree) {
-
+    let negatives = 0
+    if (numOne < 0) {
+        negatives += 1
+    }
+    if (numTwo < 0) {
+        negatives += 1
+    }
+    if (numThree < 0) {
+        negatives += 1
+    }
+    if (negatives % 2 === 0){
+        return 'Positive'
+    }
+    return 'Negative'
 }
+
+console.log(
+    singCheck(1, -2 , -3)
+)
