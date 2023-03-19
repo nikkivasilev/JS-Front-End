8447d2b54a6dc1fd1fcae70d3febdf9aa0216a2d
nikkivasilev <n.vasilev105@gmail.com>
2023-03-19 20:44:26 +0200

    playing around with more advanced DOM manipulations

--- 04. DOM and Events - More Exercises/04. Dynamic-Validation/app.js
+++ 04. DOM and Events - More Exercises/04. Dynamic-Validation/app.js
@@ -1,3 +1,18 @@
 function validate() {
-    console.log('TODO:...');
+    const inputField = document.querySelector(`#email`)
+    inputField.addEventListener(`focusout`, focusEventAction)
+
+    function focusEventAction(e) {
+        let email = e.target.value
+        if (!validateEmail(email)) {
+            e.target.className = `error`
+        } else if (validateEmail(email)) {
+            e.target.className = ``
+        }
+    }
+    function validateEmail(email) {
+        const regex = /^[a-z]+@[a-z]+\.[a-z]+$/;
+        return regex.test(email);
+      }
 }
+
--- 04. DOM and Events - More Exercises/05. Shopping-Cart/solution.js
+++ 04. DOM and Events - More Exercises/05. Shopping-Cart/solution.js
@@ -1,3 +1,30 @@
 function solve() {
-   //TODO...
+   let totalPrice = 0
+   let boughtProducts = {}
+   let resultTextarea = document.getElementsByTagName(`textarea`)[0]
+   let addButtons = Array.from(document.querySelectorAll(`.add-product`))
+   let checkoutButton = document.querySelector(`.checkout`)
+   addButtons.forEach(button =>{
+      button.addEventListener('click', addProductHandler)
+   })
+   checkoutButton.addEventListener(`click`, checkoutHandler)
+   function addProductHandler (event) {
+      let button = event.target
+      let productElement = button.parentElement.parentElement
+      let productName = productElement.querySelector(`.product-details .product-title`).textContent
+      let productPrice = Number(productElement.querySelector(`.product-line-price`).textContent)
+      totalPrice += productPrice
+      boughtProducts[productName] = 1
+      let resultText = `Added ${productName} for ${productPrice.toFixed(2)} to the cart.\n`
+      resultTextarea.textContent += resultText
+   }
+
+   function checkoutHandler (e) {
+      resultTextarea.textContent += `You bought ${Object.keys(boughtProducts).join(`, `)} for ${totalPrice.toFixed(2)}.`
+      addButtons.forEach((button) => {
+         button.disabled = true
+      })
+      e.target.disabled = true
+   }
+   
 }
