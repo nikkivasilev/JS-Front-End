69c63d7660bda629e95efd34726f3a8723dba601
nikkivasilev <n.vasilev105@gmail.com>
2023-03-21 22:20:26 +0200

    playing around with more advanced DOM manipulations and HTTP protocols

--- 05. JS Front End HTTP and REST/04. GitHub-Repos/scripts/app.js
+++ 05. JS Front End HTTP and REST/04. GitHub-Repos/scripts/app.js
@@ -1,3 +1,12 @@
 function loadRepos() {
-   console.log("TODO...");
+   const BASE_URL = `https://api.github.com/users/testnakov/repos`
+   const resultContainer = document.getElementById(`res`)
+   fetch(BASE_URL, {method: `GET`})
+      .then((res) => res.text())
+      .then((data) =>{
+         resultContainer.textContent = data
+      })
+      .catch((err) => {
+         console.log(err);
+      })
 }
