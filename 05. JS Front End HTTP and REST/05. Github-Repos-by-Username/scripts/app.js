6eb409d29bb38afae82452f167bd6eafd34231b1
nikkivasilev <n.vasilev105@gmail.com>
2023-03-22 18:56:36 +0200

    playing around with more advanced DOM manipulations and HTTP protocols

--- 05. JS Front End HTTP and REST/05. Github-Repos-by-Username/scripts/app.js
+++ 05. JS Front End HTTP and REST/05. Github-Repos-by-Username/scripts/app.js
@@ -1,3 +1,23 @@
-function loadRepos() {
-	console.log("TODO...");
+async function loadRepos() {
+	const BASE_URL = `https://api.github.com/users`
+	const username = document.getElementById(`username`).value
+	const reposList = document.getElementById(`repos`)
+	reposList.children[0].remove()
+	
+	try{
+	const response = await(fetch(`${BASE_URL}/${username}/repos`))
+	const userRepos = await response.json()
+	userRepos.forEach(repo => {
+		let liItem = document.createElement(`li`)
+		liItem.textContent = repo.full_name
+		reposList.appendChild(liItem)
+
+
+	})
+	}
+	catch {
+		let liItem = document.createElement(`li`)
+		liItem.textContent = `Not Found`
+		reposList.appendChild(liItem)
+	}
 }
